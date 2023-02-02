2415b094760894d2de7ef7f8bbf2d195cf73a303
Domains18 <gibsonsgibson88@gmail.com>
2023-02-02 19:30:07 +0300

    server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+const cors = require("cors");
+const colors = require("colors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+//app
+const server = express()
+
+
+// middlewares and functions
+server.use(cors());
+
+server.listen(PORT, () => { console.log(`server started on port ${PORT}`) });
