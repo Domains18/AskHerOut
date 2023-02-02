f751784c4225d4f3085e1fce32bbf767aca63d3d
Domains18 <gibsonsgibson88@gmail.com>
2023-02-02 21:29:21 +0300

    router sketch

--- routes/studentRegistration.js
+++ routes/studentRegistration.js
@@ -5,5 +5,10 @@ router.post('/', async (req, res) => {
     res.status(200).json({message: "this is register route"});
 });
 
-
+router.post('/login', async (req, res) => {
+    res.status(200).json({ message: " this is the login route" });
+})
+router.get('/me', async (req, res) => {
+    res.status(200).json({ message: "this is aboutMe route"});
+});
 module.exports = router;
