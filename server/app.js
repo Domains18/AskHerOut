2c3e2b37ee9c9b60ff1fc9bbe9b8962daeaf64b5
Domains18 <gibsonsgibson88@gmail.com>
2023-05-03 19:30:35 +0300

    server
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//create todo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(5000, () => {
+    console.log('server has started on port 5000');
+});
