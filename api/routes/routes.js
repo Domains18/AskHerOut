ab61f12f7889e896501313e7fc3a45b9767bad25
Domains18 <gibsonsgibson88@gmail.com>
2023-05-03 12:18:46 +0300

    file cleanup
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- api/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.route('/').get((req, res) => {
-    res.send('Backend API is working!');
-});
-    
-module.exports = router;
