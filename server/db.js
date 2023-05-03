85820ce6f78be062024e15c6aaab2a5e050c9649
Domains18 <gibsonsgibson88@gmail.com>
2023-05-03 13:49:45 +0300

    database
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- server/db.js
+++ server/db.js
@@ -1,1 +1,13 @@
 
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "domains18",
+    password: "alphauser",
+    port: 5432,
+    database: 'perntodo'
+})
+
+
+
+module.exports = pool;
